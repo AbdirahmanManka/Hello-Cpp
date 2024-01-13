11ba2e58404cbba77fdbc868d034db25d828152a
Abdirahman <114359001+OmarAbdirahman@users.noreply.github.com>
2024-01-14 01:21:11 +0300

    for loop in c++

---
+++ for.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    for (int i = 0; i < 10; i++) {
+        std::cout << "Hello Waryaa" << std::endl;
+    }
+    for (int i = 0; i < 10; i++) {
+        std::cout << "Come here " << std::endl;
+    }
+    return 0;
+}
