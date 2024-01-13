e80176dcadcc2687d4729e3fb87f1931d73455d9
Abdirahman <114359001+OmarAbdirahman@users.noreply.github.com>
2024-01-13 08:14:09 +0300

    constants in c++

---
+++ constants.cpp
@@ -0,0 +1,13 @@
+// cpp constants
+#include <iostream>
+using namespace std;
+//constants are like variables except that their value cannot be changed after their definition.
+//To define a constant, we use the const keyword.
+//To use constants, we have to declare them first.
+
+
+int main() {
+  const int myNum = 15;
+  cout << myNum;
+  return 0;
+}
